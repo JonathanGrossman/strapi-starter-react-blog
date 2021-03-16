e3145a53ec67422d162a731f9fddfb9b9e4dee4f
JonathanGrossmanITC <jonathan@itc.tech>
2021-03-16 19:34:34 +0200

    added botfront

--- src/containers/Articles/index.js
+++ src/containers/Articles/index.js
@@ -2,13 +2,26 @@ import React from "react";
 import Articles from "../../components/Articles";
 import Query from "../../components/Query";
 import ARTICLES_QUERY from "../../queries/article/articles";
+import Widget from "rasa-webchat";
 
 const Home = () => {
+  const CustomWidget = () => {
+    return (
+      <Widget
+        initPayload={"/get_started"}
+        socketUrl={"http://localhost:5500"}
+        socketPath={"/socket.io/"}
+        customData={{ language: "en" }} // arbitrary custom data. Stay minimal as this will be added to the socket
+        title={"Title"}
+      />
+    );
+  };
   return (
     <div>
       <div className="uk-section">
         <div className="uk-container uk-container-large">
           <h1>The Land of Ozbot</h1>
+          <CustomWidget />
           <Query query={ARTICLES_QUERY}>
             {({ data: { articles } }) => {
               return <Articles articles={articles} />;
